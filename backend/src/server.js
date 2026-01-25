475a0ecf265b46ecd908023664b0a27a7acfe9d0
EK-TOP <koivunen.emil@gmail.com>
2026-01-25 02:08:33 +0200

    korttimodel ja korttiroutes valmis ja testattu

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 
-// backend/src/server.js
+//backend/src/server.js
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 
 import asiakasRoutes from './routes/asiakasRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import korttiRoutes from './routes/korttiRoutes.js';
 import authenticateToken from './middleware/authenticateToken.js';
 
 dotenv.config();
@@ -23,13 +24,9 @@ app.get('/health', (req, res) => res.json({ ok: true }));
 
 
 app.use(authenticateToken);
+app.use('/kortti', korttiRoutes);
 app.use('/asiakas', asiakasRoutes);
 
-
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`API listening on ${port}`));
-
 const host = '0.0.0.0';
 app.listen(port, host, () => console.log(`API listening on ${host}:${port}`));
